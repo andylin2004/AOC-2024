bdb4ec0cc34cf4875163217d0be26b094b8c8aee
Andy Lin <andy.lin.2004@outlook.com>
2024-12-01 05:20:31 +0000

    day01 done

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -6,20 +6,68 @@ struct Day01: AdventDay {
 
   // Splits input data into its component parts and convert from string.
   var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+    data.split(separator: "\n").map {
+      $0.split(separator: " ").compactMap { Int($0) }
     }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    
+    var left: [Int] = {
+      entities.compactMap { a in
+        a[0]
+      }.sorted().reversed()
+    }()
+    
+    var right: [Int] = {
+      entities.compactMap { a in
+        a[1]
+      }.sorted().reversed()
+    }()
+    
+    var total = 0
+    
+    while !left.isEmpty || !right.isEmpty {
+      let a = left.popLast() ?? 0
+      let b = right.popLast() ?? 0
+      
+      total += abs(a-b)
+    }
+    
+    return total
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    let left: [Int] = {
+      entities.compactMap { a in
+        a[0]
+      }
+    }()
+    
+    let right: [Int] = {
+      entities.compactMap { a in
+        a[1]
+      }.sorted().reversed()
+    }()
+    
+    var rightDict = [Int: Int]()
+    
+    for i in right {
+      if rightDict.keys.contains(i) {
+        rightDict[i]! += 1
+      } else {
+        rightDict[i] = 1
+      }
+    }
+    
+    var total = 0
+    
+    for i in left {
+      total += (rightDict[i] ?? 0) * i
+    }
+    
+    return total
   }
 }
