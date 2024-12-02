38cf00f79949fbb35404bb8b7f8b710fdf48b394
Andy Lin <andy.lin.2004@outlook.com>
2024-12-02 09:38:11 +0000

    day 2 that was horrible

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -1,6 +1,6 @@
 import Algorithms
 
-struct Day01: AdventDay {
+struct Day02: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
@@ -13,59 +13,53 @@ struct Day01: AdventDay {
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    
-    var left: [Int] = {
-      entities.compactMap { a in
-        a[0]
-      }.sorted().reversed()
-    }()
-    
-    var right: [Int] = {
-      entities.compactMap { a in
-        a[1]
-      }.sorted().reversed()
-    }()
-    
     var total = 0
-    
-    while !left.isEmpty || !right.isEmpty {
-      let a = left.popLast() ?? 0
-      let b = right.popLast() ?? 0
+    for row in entities {
+      var direction: Int = 0
+      for i in 1..<row.count {
+        let diff = row[i] - row[i-1]
+        if (1 <= diff && diff <= 3 && direction >= 0) || (-3 <= diff && diff <= -1 && direction <= 0) {
+          direction = diff
+          if i == row.count - 1 {
+            total += 1
+          }
+        } else {
+          break
+        }
+      }
       
-      total += abs(a-b)
     }
-    
     return total
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    let left: [Int] = {
-      entities.compactMap { a in
-        a[0]
-      }
-    }()
-    
-    let right: [Int] = {
-      entities.compactMap { a in
-        a[1]
-      }.sorted().reversed()
-    }()
-    
-    var rightDict = [Int: Int]()
-    
-    for i in right {
-      if rightDict.keys.contains(i) {
-        rightDict[i]! += 1
-      } else {
-        rightDict[i] = 1
-      }
-    }
-    
     var total = 0
     
-    for i in left {
-      total += (rightDict[i] ?? 0) * i
+    for row in entities {
+      var add = false
+      for combo in row.combinations(ofCount: row.count - 1) {
+        var direction: Int = 0
+        for i in 1..<combo.count {
+          let diff = combo[i] - combo[i-1]
+          if (1 <= diff && diff <= 3 && direction >= 0) || (-3 <= diff && diff <= -1 && direction <= 0) {
+            direction = diff
+            if i == combo.count - 1 {
+              add = true
+              break
+            }
+          } else {
+            break
+          }
+        }
+        if add {
+          break
+        }
+      }
+      
+      if add {
+        total += 1
+      }
     }
     
     return total
