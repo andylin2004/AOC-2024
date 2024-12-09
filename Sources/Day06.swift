2d03507a4032f04f978d4246ae00d0a69e596c81
Andy Lin <andy.lin.2004@outlook.com>
2024-12-09 01:24:46 +0100

    day 6 done

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -1,126 +1,208 @@
 import Algorithms
 
-struct Day05: AdventDay {
+struct Day06: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
   
-  var splitData: [Substring] {
-    data.split(separator: "\n\n").compactMap {
-      $0
+  var map: [[Character]] {
+    data.split(separator: "\n").compactMap {
+      Array($0)
     }
   }
-  
-  var rules: [(Int, Int)] {
-    splitData[0].split(separator: "\n").compactMap { line in
-      let splitLine = line.split(separator: "|").compactMap { half in
-        Int(half)
-      }
-      return (splitLine[0], splitLine[1])
-    }
-  }
-  
-  var pagesSetProduced: [[Int]] {
-    splitData[1].split(separator: "\n").compactMap { line in
-      line.split(separator: ",").compactMap { item in
-        Int(item)
-      }
-    }
-  }
-  
-  var kvPairsAfter = [Int: [Int]]()
-  var kvPairsBefore = [Int: [Int]]()
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    var total = 0
+    var curRow = map.firstIndex(where: {$0.contains(where: {$0 == "^"})}) ?? 0
+    var curCol = map[curRow].firstIndex(of: "^") ?? 0
+    var direction = Direction.up
+    var positions: Set<Location> = [Location(row: curRow, col: curCol)]
     
-    for pagesSetProduce in pagesSetProduced {
-      for i in 0..<pagesSetProduce.count {
-        if ({
-          if i+1 < pagesSetProduce.count {
-            return Set(pagesSetProduce[i+1..<pagesSetProduce.count]).isSubset(of: Set(kvPairsAfter[pagesSetProduce[i]] ?? []))
-          } else {
-            return true
-          }
-        }() && {
-          if 0 <= i - 1 {
-            return Set(pagesSetProduce[0...i-1]).isSubset(of: Set(kvPairsBefore[pagesSetProduce[i]] ?? []))
-          } else {
-            return true
-          }
-        }()) {
-          if i == pagesSetProduce.count - 1 {
-            total += pagesSetProduce[pagesSetProduce.count / 2]
-          }
-        } else {
-          break
-        }
+    while 0 <= curRow && curRow < map.count && 0 <= curCol && curCol < map[0].count {
+      if let next = step(map: map, direction: &direction, curRow: &curRow, curCol: &curCol) {
+        positions.insert(next)
+      } else {
+        break
       }
     }
     
-    return total
+    return positions.count
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
+    var curRow = map.firstIndex(where: {$0.contains(where: {$0 == "^"})}) ?? 0
+    var curCol = map[curRow].firstIndex(of: "^") ?? 0
+    var direction = Direction.up
     var total = 0
+    var steps: Set<Location> = []
     
-    for pagesSetProduce in pagesSetProduced {
-      for i in 0..<pagesSetProduce.count {
-        if ({
-          if i+1 < pagesSetProduce.count {
-            return Set(pagesSetProduce[i+1..<pagesSetProduce.count]).isSubset(of: Set(kvPairsAfter[pagesSetProduce[i]] ?? []))
-          } else {
-            return true
+    while 0 <= curRow && curRow < map.count && 0 <= curCol && curCol < map[0].count {
+      if canContinue(map: map, direction: direction, curRow: curRow, curCol: curCol) == .yes && canTurnAndCollide(map: map, direction: direction, curRow: curRow, curCol: curCol){
+        let blockLoc = nextBlock(direction: direction, curRow: curRow, curCol: curCol)
+        if !steps.contains(blockLoc) {
+          var tempMap = map
+          tempMap[blockLoc.row][blockLoc.col] = "#"
+          if turnCausesALoop(map: tempMap, direction: direction, curRow: curRow, curCol: curCol) {
+            total += 1
           }
-        }() && {
-          if 0 <= i - 1 {
-            return Set(pagesSetProduce[0...i-1]).isSubset(of: Set(kvPairsBefore[pagesSetProduce[i]] ?? []))
-          } else {
-            return true
-          }
-        }()) {
-        } else {
-          var correctedOrder = [Int]()
-          var setRemaining = Set(pagesSetProduce)
-          
-          while correctedOrder.count < pagesSetProduce.count {
-            let permutations = setRemaining.combinations(ofCount: setRemaining.count - 1)
-            
-            for permutation in permutations {
-              let leadNumber = Set(setRemaining).symmetricDifference(Set(permutation)).first!
-              
-              if Set(permutation).isSubset(of: Set(kvPairsAfter[leadNumber] ?? [])) && Set(correctedOrder[0..<(correctedOrder.count-1 > 0 ? correctedOrder.count-1 : 0)]).isSubset(of: Set(kvPairsBefore[leadNumber] ?? [])) {
-                correctedOrder.append(leadNumber)
-                setRemaining.remove(leadNumber)
-                break
-              }
-            }
-          }
-          total += correctedOrder[correctedOrder.count / 2]
-          break
         }
+      }
+      
+      if let step = step(map: map, direction: &direction, curRow: &curRow, curCol: &curCol) {
+        steps.insert(step)
+      } else {
+        break
       }
     }
     
     return total
   }
   
-  init(data: String) {
-    self.data = data
-    
-    for rule in rules {
-      if let _ = kvPairsAfter[rule.0] {
-        kvPairsAfter[rule.0]?.append(rule.1)
-      } else {
-        kvPairsAfter[rule.0] = [rule.1]
-      }
-      
-      if let _ = kvPairsBefore[rule.1] {
-        kvPairsBefore[rule.1]?.append(rule.0)
-      } else {
-        kvPairsBefore[rule.1] = [rule.0]
+  enum Direction: Int {
+    case up = 0
+    case down = 2
+    case left = 3
+    case right = 1
+  }
+  
+  struct Location: Hashable {
+    let row: Int
+    let col: Int
+  }
+  
+  struct LocationDirection: Hashable {
+    let location: Location
+    let direction: Direction
+  }
+  
+  func step(map: [[Character]], direction: inout Direction, curRow: inout Int, curCol: inout Int) -> Location? {
+    do {
+      switch canContinue(map: map, direction: direction, curRow: curRow, curCol: curCol) {
+      case .ifTurned:
+        direction = Direction(rawValue: (direction.rawValue + 1) % 4)!
+        return Location(row: curRow, col: curCol)
+      case .no:
+        return nil
+      case .yes:
+        return move(direction: direction, curRow: &curRow, curCol: &curCol)
       }
     }
+  }
+  
+  func canContinue(map: [[Character]], direction: Direction, curRow: Int, curCol: Int) -> CanContinue {
+    switch direction {
+    case .up:
+      if 0 <= curRow - 1 {
+        return map[curRow - 1][curCol] != "#" ? .yes : .ifTurned
+      } else {
+        return .no
+      }
+    case .down:
+      if curRow + 1 < map.count {
+        return map[curRow + 1][curCol] != "#" ? .yes : .ifTurned
+      } else {
+        return .no
+      }
+    case .left:
+      if 0 <= curCol - 1 {
+        return map[curRow][curCol - 1] != "#" ? .yes : .ifTurned
+      } else {
+        return .no
+      }
+    case .right:
+      if curCol + 1 < map[0].count {
+        return map[curRow][curCol + 1] != "#" ? .yes : .ifTurned
+      } else {
+        return .no
+      }
+    }
+  }
+  
+  func move(direction: Direction, curRow: inout Int, curCol: inout Int) -> Location {
+    switch direction {
+    case .up:
+      curRow -= 1
+    case .down:
+      curRow += 1
+    case .left:
+      curCol -= 1
+    case .right:
+      curCol += 1
+    }
+    return Location(row: curRow, col: curCol)
+  }
+           
+  func nextBlock(direction: Direction, curRow: Int, curCol: Int) -> Location{
+    switch direction {
+    case .up:
+      return Location(row: curRow - 1, col: curCol)
+    case .down:
+      return Location(row: curRow + 1, col: curCol)
+    case .left:
+      return Location(row: curRow, col: curCol - 1)
+    case .right:
+      return Location(row: curRow, col: curCol + 1)
+    }
+  }
+  
+  func canTurnAndCollide(map: [[Character]], direction: Direction, curRow: Int, curCol: Int) -> Bool {
+    let newDirection: Direction = Day06.Direction(rawValue: (direction.rawValue + 1) % 4)!
+    
+    switch newDirection {
+    case .up:
+      for row in 0..<curRow {
+        if map[row][curCol] == "#" {
+          return true
+        }
+      }
+    case .down:
+      for row in curRow + 1..<map.count {
+        if map[row][curCol] == "#" {
+          return true
+        }
+      }
+    case .left:
+      for col in 0..<curCol {
+        if map[curRow][col] == "#" {
+          return true
+        }
+      }
+    case .right:
+      for col in curCol..<map[0].count {
+        if map[curRow][col] == "#" {
+          return true
+        }
+      }
+    }
+    
+    return false
+  }
+  
+  enum CanContinue {
+    case ifTurned
+    case no
+    case yes
+  }
+  
+  func turnCausesALoop(map: [[Character]], direction: Direction, curRow: Int, curCol: Int) -> Bool {
+    var steps: Set<LocationDirection> = []
+    var direction = Direction(rawValue: (direction.rawValue + 1) % 4)!
+    var curRow = curRow
+    var curCol = curCol
+    
+    while 0 <= curRow && curRow < map.count && 0 <= curCol && curCol < map[0].count {
+      let oldCount = steps.count
+      if let next = step(map: map, direction: &direction, curRow: &curRow, curCol: &curCol) {
+        let newStep = LocationDirection(location: next, direction: direction)
+        steps.insert(newStep)
+        if oldCount == steps.count {
+          return true
+        }
+      } else {
+        break
+      }
+    }
+    
+    return false
   }
 }
